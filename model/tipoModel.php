5373a6155bd81b86ce8d80e0d399424bd0251450
ibaia <achita145@gmail.com>
2020-02-28 18:22:40 +0100

    Modelos semicompletos, falta getList y funciones especificas

---
+++ model/tipoModel.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/Destila2CocktailLounge/model/connect_data.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/Destila2CocktailLounge/model/tipoClass.php';
+
+class tipoModel extends tipoClass{
+    
+    private $list=array();
+    private $link;
+    
+    //Conexion
+    public function OpenConnect(){
+        
+        $konDat=new connect_data();
+        try
+        {
+            $this->link=new mysqli($konDat->host,$konDat->userbbdd,$konDat->passbbdd,$konDat->ddbbname);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+        $this->link->set_charset("utf8"); // honek behartu egiten du aplikazio eta
+        //                  //databasearen artean UTF -8 erabiltzera datuak trukatzeko
+    }
+    public function CloseConnect(){
+        
+        mysqli_close ($this->link);
+        
+    }
+    
+    //Link
+    public function getLink(){
+        
+        return $this->link;
+    }
+    /**
+     * @param mysqli $link
+     */
+    public function setLink($link){
+        
+        $this->link = $link;
+        
+    }
+    
+    //coger la lista
+    public function getList(){
+        
+        return $this->list;
+    }
+    
+    //Crear La lista
+    public function setList() {
+        
+        $this->OpenConnect();
+        
+        $sql=")";
+        
+        //mysqli_free_result($result);
+        //$this->CloseConnect();
+    }
+    
+    
+    
+    
+    
+}
