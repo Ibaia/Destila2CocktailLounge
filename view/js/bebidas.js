0da8a3cf68e950066baebeb02a05b920403f61a8
deathbladebass <deathbladebass@gmail.com>
2020-03-03 17:12:59 +0100

    prep

---
+++ view/js/bebidas.js
@@ -0,0 +1,14 @@
+/* <h2>Nuestros packs</h2>
+            <div class="row">
+              <div class="col-12 col-sm-3">
+                <img class="imgIndex shadow-sm" src="img/fuegovalyrio.jpg">
+              </div>
+              <div class=" col-sm-9 col-12">
+                <h4>pack 1</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi modi consequuntur aliquam ad iusto reiciendis qui architecto animi aspernatur perferendis! A vel modi aut tempora explicabo? Enim blanditiis adipisci totam?</p>
+              </div>
+            </div> */
+
+$( document ).ready(function() {
+    
+});
